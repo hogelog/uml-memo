c2d58d81d0cde32fb7667d6f54a17e19e33b9c76
Sunao Komuro <sunao-komuro@cookpad.com>
2019-09-10 15:53:49 +0900

    Add /hello endpoint

---
+++ src/main/java/uml/memo/HelloController.java
@@ -0,0 +1,14 @@
+package uml.memo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/hello", method = GET)
+    public String hello() {
+        return "hello";
+    }
+}
