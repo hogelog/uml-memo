c03e3073fef87e9f5b3bfb43d7b56bea70371a42
hogelog <konbu.komuro@gmail.com>
2019-09-10 21:20:09 +0900

    Add test

---
+++ src/test/java/uml/memo/UmlControllerTest.java
@@ -0,0 +1,29 @@
+package uml.memo;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UmlControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getImage() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/uml/image").accept(MediaType.IMAGE_PNG_VALUE))
+                .andExpect(status().isOk());
+    }
+}
